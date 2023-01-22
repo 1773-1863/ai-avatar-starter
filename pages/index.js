3e938247295704cee1ede5ef05e575355b7c335e
Enes Hakan <96254574+1773-1863@users.noreply.github.com>
2023-01-22 16:11:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,33 +4,39 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
   return (
-    <div className="root">
-      <Head>
-        <title>AI Avatar Generator | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>your generator one-liner</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>description of your generator</h2>
-          </div>
-        </div>
+<div className="root">
+  <Head>
+    <title>Silly picture generator | buildspace</title>
+  </Head>
+  <div className="container">
+    <div className="header">
+      <div className="header-title">
+        <h1>Silly picture generator</h1>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
+      <div className="header-subtitle">
+        <h2>
+          Turn me into anyone you want! Make sure you refer to me as "abraza" in the prompt
+        </h2>
+      </div>
+      {/* Add prompt container here */}
+      <div className="prompt-container">
+        <input className="prompt-box" />
       </div>
     </div>
+  </div>
+  <div className="badge-container grow">
+    <a
+      href="https://buildspace.so/builds/ai-avatar"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="badge">
+        <Image src={buildspaceLogo} alt="buildspace logo" />
+        <p>build with buildspace</p>
+      </div>
+    </a>
+  </div>
+</div>
   );
 };
 
